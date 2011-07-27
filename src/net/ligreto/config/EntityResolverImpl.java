a1cb6af27c7070e3fd741d2d5f9291ea7ab35796
Julius.Stroffek <Julius.Stroffek@localhost>
2011-07-27 20:05:56 +0530

    * Correctly using the DTD stored in jar file.

--- src/net/ligreto/config/EntityResolverImpl.java
+++ src/net/ligreto/config/EntityResolverImpl.java
@@ -1,6 +1,7 @@
 package net.ligreto.config;
 
 import java.io.IOException;
+import java.io.InputStream;
 
 import org.xml.sax.EntityResolver;
 import org.xml.sax.InputSource;
@@ -11,9 +12,11 @@ public class EntityResolverImpl implements EntityResolver {
 	@Override
 	public InputSource resolveEntity(String publicId, String systemId)
 			throws SAXException, IOException {
-		if ("ligreto.dtd".equals(publicId)) {
+		if ("-//ligreto.dtd".equals(publicId)) {
 			EntityResolverImpl.class.getClassLoader();
-			return new InputSource(ClassLoader.getSystemResourceAsStream("ligreto.dtd"));
+			InputStream stream = ClassLoader.getSystemResourceAsStream("resources/ligreto.dtd");
+			InputSource result = new InputSource(stream);
+			return result;
 		}
 		return null;
 	}
