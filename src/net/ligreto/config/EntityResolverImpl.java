c7d0157110e46a817fb035a0a760e7b7f50c5c3c
Julius.Stroffek <Julius.Stroffek@localhost>
2011-07-27 20:15:39 +0530

    * Simplifying the PublicId and SystemId in DTD. * Fixed the Eclipse class not found errors.

--- src/net/ligreto/config/EntityResolverImpl.java
+++ src/net/ligreto/config/EntityResolverImpl.java
@@ -12,7 +12,7 @@ public class EntityResolverImpl implements EntityResolver {
 	@Override
 	public InputSource resolveEntity(String publicId, String systemId)
 			throws SAXException, IOException {
-		if ("-//ligreto.dtd".equals(publicId)) {
+		if ("ligreto.dtd".equals(publicId)) {
 			EntityResolverImpl.class.getClassLoader();
 			InputStream stream = ClassLoader.getSystemResourceAsStream("resources/ligreto.dtd");
 			InputSource result = new InputSource(stream);
